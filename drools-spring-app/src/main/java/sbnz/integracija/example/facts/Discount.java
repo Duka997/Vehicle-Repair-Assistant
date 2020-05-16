4083265640eb12cb0121fc87469c2ffb62f37390
Dusan <dusan.dsavic.97@gmail.com>
2020-05-16 15:47:40 +0200

    model Discount added

---
+++ drools-spring-app/src/main/java/sbnz/integracija/example/facts/Discount.java
@@ -0,0 +1,27 @@
+package sbnz.integracija.example.facts;
+
+public class Discount {
+	
+	private Double amount;
+
+	public Discount(Double amount) {
+		super();
+		this.amount = amount;
+	}
+
+	public Discount() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	
+
+}
